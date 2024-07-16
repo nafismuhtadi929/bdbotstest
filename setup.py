765cbd1ae8ea8f6004c2898530843d7e775620ef
nafismuhtadi929 <147165994+nafismuhtadi929@users.noreply.github.com>
2024-07-16 15:27:16 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     description='A bdbots test module',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/mymodule',
+    url='https://github.com/nafismuhtadi929/bdbotstest',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
